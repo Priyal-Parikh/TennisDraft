7406870ca289fe1eaa2d85391eb66d7268fd2414
Priyal Parikh <parikhpriyal91@yahoo.com>
2020-08-24 16:07:42 +0530

    Add: Start new tennis game with two players

--- src/main/java/com/bnppf/kata/TennisGame.java
+++ src/main/java/com/bnppf/kata/TennisGame.java
@@ -1,4 +1,19 @@
 package com.bnppf.kata;
 
 public class TennisGame {
+    private String firstPlayerName;
+    private String secondPlayerName;
+
+    public TennisGame(String firstPlayerName , String secondPlayerName) {
+        this.firstPlayerName = firstPlayerName;
+        this.secondPlayerName = secondPlayerName;
+    }
+
+    public String getFirstPlayerName() {
+        return firstPlayerName;
+    }
+
+    public String getSecondPlayerName() {
+        return secondPlayerName;
+    }
 }
--- src/test/java/com/bnppf/kata/TennisGameTest.java
+++ src/test/java/com/bnppf/kata/TennisGameTest.java
@@ -6,8 +6,16 @@ import org.junit.Test;
 public class TennisGameTest {
     @Test
     public void initializeNewTennisGame() {
-        TennisGame tennisGame = new TennisGame();
+        TennisGame tennisGame = new TennisGame("Serena Williams" , "Maria Sharapova");
 
         Assert.assertNotNull(tennisGame);
+    }
+
+    @Test
+    public void startNewGameWithTwoPlayers() {
+        TennisGame tennisGame = new TennisGame("Serena Williams" , "Maria Sharapova");
+
+        Assert.assertEquals("Serena Williams" , tennisGame.getFirstPlayerName());
+        Assert.assertEquals("Maria Sharapova" , tennisGame.getSecondPlayerName());
     }
 }
